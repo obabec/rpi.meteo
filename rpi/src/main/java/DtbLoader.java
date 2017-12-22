bb6b641d1eb8f9b47705dae31242ca215755196b
obabec <ond.babec@gmail.com>
2017-12-22 15:53:05 +0100

    Initial commit

---
+++ rpi/src/main/java/DtbLoader.java
@@ -0,0 +1,41 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.Timestamp;
+import java.util.Calendar;
+
+public class DtbLoader {
+
+    public void loadAndPush(Double temp,Double hum){
+        try {
+
+            Class.forName("com.mysql.jdbc.Driver");
+            // Setup the connection with the DB
+            Connection connect = DriverManager
+                    .getConnection("jdbc:mysql://localhost/testDtb?"
+                            + "user=root&password=95123Lo");
+            String query = " insert into tableTest (temp,hum,dates)"
+                    + " values (?,?,?)";
+
+            PreparedStatement preparedStmt = connect.prepareStatement(query);
+            preparedStmt.setDouble(1,temp);
+            preparedStmt.setDouble(2,hum);
+            preparedStmt.setTimestamp(3,currentDate());
+
+            preparedStmt.execute();
+            connect.close();
+        } catch (Exception e) {
+            System.err.println("Got an exception!");
+            System.err.println(e.getMessage());
+        }
+    }
+    private Timestamp currentDate(){
+
+        long currentTime = Calendar.getInstance().getTimeInMillis();
+        Timestamp ts = new Timestamp(currentTime);
+
+        return ts;
+    }
+
+
+}
---
+++ rpi/src/main/java/Main.java
@@ -0,0 +1,63 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+public class Main {
+
+    public static void main(String[] args) throws InterruptedException {
+
+        String s = null;
+        String result = null;
+        DtbLoader dtbWork = new DtbLoader();
+
+        while (true){
+            try {
+
+                // run the Unix "ps -ef" command
+                // using the Runtime exec method:
+                Process p = Runtime.getRuntime().exec("python test.py");
+
+                BufferedReader stdInput = new BufferedReader(new
+                        InputStreamReader(p.getInputStream()));
+
+                BufferedReader stdError = new BufferedReader(new
+                        InputStreamReader(p.getErrorStream()));
+
+                // read the output from the command
+                System.out.println("Here is the standard output of the command:\n");
+                while ((s = stdInput.readLine()) != null) {
+                    System.out.println(s);
+                    if (!s.isEmpty()){
+                        result = s;
+                    }
+
+                }
+
+                // read any errors from the attempted command
+                System.out.println("Here is the standard error of the command (if any):\n");
+                while ((s = stdError.readLine()) != null) {
+                    System.out.println(s);
+                }
+
+
+                dtbWork.loadAndPush(Double.valueOf(result.split(";")[0]),Double.valueOf(result.substring(result.lastIndexOf(";")+1)));
+                System.exit(0);
+            }
+            catch (IOException e) {
+                System.out.println("exception happened - here's what I know: ");
+                e.printStackTrace();
+                System.exit(-1);
+            }
+
+            Thread.sleep(4000);
+        }
+
+
+    }
+}
